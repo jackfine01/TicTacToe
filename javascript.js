73846b0db40bb3e707a63c15479f9aa1763f283f
jack <jackfine01@gmail.com>
2024-12-11 15:15:16 -0500

    added winning logic

--- javascript.js
+++ javascript.js
@@ -26,34 +26,62 @@ function playRound(human, computer){
         }
         else if(humanChoice === "Rock" && computerChoice === "Scissors"){
             document.getElementById("descriptorOfResult").textContent = ("You win! Rock beats Scissors.");
+            document.getElementById("winner").textContent = "";
             humanScore++;
         }
         else if(computerChoice === "Rock" && humanChoice === "Scissors"){
             document.getElementById("descriptorOfResult").textContent = ("You lose! Rock beats Scissors.");
+            document.getElementById("winner").textContent = "";
             computerScore++;
         }
         else if(humanChoice === "Paper" && computerChoice === "Scissors"){
             document.getElementById("descriptorOfResult").textContent = ("You lose! Scissors beats Paper.");
+            document.getElementById("winner").textContent = "";
             computerScore++;
         }
         else if(computerChoice === "Paper" && humanChoice === "Scissors"){
             document.getElementById("descriptorOfResult").textContent = ("You win! Scissors beats Paper.");
+            document.getElementById("winner").textContent = "";
             humanScore++;
         }
         else if(computerChoice === "Paper" && humanChoice === "Rock"){
             document.getElementById("descriptorOfResult").textContent = "You Lose! Paper beats Rock.";
+            document.getElementById("winner").textContent = "";
             computerScore++;
         }
         else if(computerChoice === "Rock" && humanChoice === "Paper") {
             document.getElementById("descriptorOfResult").textContent = "You win! Paper beats Rock.";
+            document.getElementById("winner").textContent = "";
             humanScore++;
         }
         else
             console.log("Invalid Result");
 
-            document.getElementById("scoreCard").textContent = humanScore + " : " + computerScore;
-            document.getElementById("playerResult").textContent = humanChoice;
-            document.getElementById("computerResult").textContent = computerChoice;
+
+        if(computerScore>2) {
+                document.getElementById("winner").textContent = "Computer";
+                document.getElementById("playerResult").textContent = humanChoice;
+                document.getElementById("computerResult").textContent = computerChoice;
+                document.getElementById("scoreCard").textContent = humanScore + " : " + "3";
+                humanScore = 0;
+                computerScore = 0; 
+        }
+        else if(humanScore>2) {
+                humanScore = 0;
+                document.getElementById("winner").textContent = "Player";
+                document.getElementById("playerResult").textContent = humanChoice;
+                document.getElementById("computerResult").textContent = computerChoice;
+                document.getElementById("scoreCard").textContent = "3" + " : " + computerScore;
+                computerScore = 0; 
+        }
+        else{
+                document.getElementById("scoreCard").textContent = humanScore + " : " + computerScore;
+                document.getElementById("playerResult").textContent = humanChoice;
+                document.getElementById("computerResult").textContent = computerChoice;
+        };
+
+
+        
 };
 
 // Choice Logic - Human Side
@@ -74,5 +102,4 @@ function playRound(human, computer){
     const scissorsButton = document.querySelectorAll(".Button")[2];
     scissorsButton.addEventListener('click', () => playRound(setHumanChoice("Scissors"), getComputerChoice()));
 
-// Header Display
 
